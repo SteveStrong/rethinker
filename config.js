0fc6490588df75bfb98b7abbc54d3a04ddb201cf
Steve Strong <sstrong@apprenticesystems.com>
2016-08-03 23:24:41 -0400

    first commit

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    rethinkdb: {
+        host: "localhost",
+        port: 28015,
+        authKey: "",
+        db: "myDatabase"
+    },
+    express: {
+        port: 3000
+    }
+}
+
